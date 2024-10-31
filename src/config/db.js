f0f7dc9480a9b167d8e2b4da3af48d64ab3fd9d9
sunilrathod098 <sunilrajputhrathod@gmail.com>
2024-10-31 16:51:30 +0530

    update database part

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,14 @@
- 
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n Database connect successfully!! DB HOST: ${connectionInstance.connection.host}`)
+    } catch (err) {
+        console.err("Database connection is faild:", err);
+        process.exit(1)
+    }
+}
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
- 
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./config/db.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`server running on http://localhost:${process.env.PORT}`)
+    })
+}).catch((err) => {
+    console.log("Database connection is faild !! ", err);
+})
+
+
+
+
+
+
+
